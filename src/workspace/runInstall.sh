8f482b739cb8af49b9962c8c55b3c7b20a599137
John Martin <johnem@us.ibm.com>
2019-05-15 14:34:15 -0500

    Add script to install stages

---
+++ src/workspace/runInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cp -R ../stages/stage1/* .
+terraform init
+terraform apply -auto-approve
+
+cp -R ../stages/stage2/* .
+terraform init
+terraform apply -auto-approve
+
+cp -R ../stages/stage3/* .
+terraform init
+terraform apply -auto-approve
