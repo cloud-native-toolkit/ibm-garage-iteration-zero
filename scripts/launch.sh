05d90dcc64a7889813a8dd8b1e537cab35af2d3d
Sean Sundberg <seansund@us.ibm.com>
2019-05-17 09:47:33 -0500

    Updates variable names and help message in package.json and launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,22 +6,25 @@ SRC_DIR="$( cd "${SCRIPT_DIR}/../src" ; pwd -P )"
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 BM_API_KEY SL_USERNAME  SL_API_KEY"
+   echo -e "\033[1;31mUsage:\033[0m $0 IBMCLOUD_API_KEY CLASSIC_USERNAME CLASSIC_API_KEY"
+   echo -e "  where \033[1;32mIBMCLOUD_API_KEY\033[0m is the IBM Cloud api key"
+   echo -e "        \033[1;32mCLASSIC_USERNAME\033[0m is the Classic Infrastructure user name or API user name (e.g. 282165_joe@us.ibm.com)"
+   echo -e "        \033[1;32mCLASSIC_API_KEY\033[0m is the Classic Infrastructure api key"
    exit 1 # Exit script after printing help
 }
 
 if [[ -n "$1" ]]; then
-   BM_API_KEY="$1"
+   IBMCLOUD_API_KEY="$1"
 fi
 if [[ -n "$2" ]]; then
-   SL_USERNAME="$2"
+   CLASSIC_USERNAME="$2"
 fi
 if [[ -n "$3" ]]; then
-   SL_API_KEY="$3"
+   CLASSIC_API_KEY="$3"
 fi
 
 # Print helpFunction in case parameters are empty
-if [[ -z "${BM_API_KEY}" ]] || [[ -z "${SL_USERNAME}" ]] || [[ -z "${SL_API_KEY}" ]]
+if [[ -z "${IBMCLOUD_API_KEY}" ]] || [[ -z "${CLASSIC_USERNAME}" ]] || [[ -z "${CLASSIC_API_KEY}" ]]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -38,9 +41,9 @@ docker run -itd --name ibm-kube-terraform \
    -v $SRC_DIR:/home/devops/src \
    -v $(pwd)/.kube:/home/devops/.kube \
    -v $(pwd)/.helm:/home/devops/.helm \
-   -e BM_API_KEY="${BM_API_KEY}" \
-   -e SL_USERNAME="${SL_USERNAME}" \
-   -e SL_API_KEY="${SL_API_KEY}" \
+   -e BM_API_KEY="${IBMCLOUD_API_KEY}" \
+   -e SL_USERNAME="${CLASSIC_USERNAME}" \
+   -e SL_API_KEY="${CLASSIC_API_KEY}" \
    ${DOCKER_IMAGE} \
    /bin/bash  > /dev/null 2>&1
 docker exec -it --workdir /home/devops/src/workspace ibm-kube-terraform terraform init > /dev/null 2>&1
