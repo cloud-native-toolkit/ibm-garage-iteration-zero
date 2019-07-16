9eba922444f7d2d3a182219f6cdb67db8f66532a
Sean Sundberg <seansund@us.ibm.com>
2019-07-16 12:44:02 -0500

    Sonarqube fixes - Fixes typo in helm template for sonarqube postgres db user - Applies initContainer logic from ibm-sonarqube helm chart to fix pv permissions issues - Stores sonarqube username and password in `sonarqube-access` secret

--- terraform/stages/stage4/tools_helm_releases/main.tf
+++ terraform/stages/stage4/tools_helm_releases/main.tf
@@ -12,11 +12,10 @@ resource "null_resource" "fetch_jenkins_helm" {
   depends_on = ["null_resource.helm_init"]
 
   provisioner "local-exec" {
-    command = "helm fetch --repo https://kubernetes-charts.storage.googleapis.com/ --untar --untardir ${path.module}/charts --version $${VERSION} $${NAME}"
+    command = "helm fetch --repo https://kubernetes-charts.storage.googleapis.com/ --untar --untardir ${path.module}/charts $${NAME}"
 
     environment = {
       NAME = "jenkins"
-      VERSION = "1.3.2"
     }
   }
 }
@@ -50,17 +49,7 @@ resource "null_resource" "jenkins_release" {
       NAMESPACE = "${var.releases_namespace}"
     }
   }
-}//resource "null_resource" "patch_jenkins_role" {
-//  depends_on = ["null_resource.jenkins_release"]
-//
-//  provisioner "local-exec" {
-//    command = "kubectl patch -n tools roles/jenkins-schedule-agents --type='json' -p='[{\"op\": \"add\", \"path\": \"/rules/0/resources/0\", \"value\": \"secrets\"}]'"
-//
-//    environment = {
-//      KUBECONFIG = "${var.iks_cluster_config_file}"
-//    }
-//  }
-//}
+}
 
 resource "null_resource" "ibmcloud_apikey_release" {
   depends_on = ["null_resource.helm_init"]
@@ -93,15 +82,27 @@ resource "null_resource" "jenkins_config_release" {
   }
 }
 
-resource "null_resource" "sonarqube_release" {
+resource "null_resource" "fetch_sonarqube_helm" {
   depends_on = ["null_resource.helm_init"]
 
   provisioner "local-exec" {
-    command = "helm template $${CHART} --namespace $${NAMESPACE} --name $${NAME} --set ingress.hosts.0=$${HOST},database.hostname=$${DATABASE_HOST},database.port=$${DATABASE_PORT},database.name=$${DATABASE_NAME},database.username=$${DATABASE_USERNAME},database.password=$${DATABASE_PASSWORD} --values $${VALUES_FILE} | kubectl apply -n $${NAMESPACE} -f -"
+    command = "helm fetch --repo https://kubernetes-charts.storage.googleapis.com/ --untar --untardir ${path.module}/charts $${NAME}"
+
+    environment = {
+      NAME = "sonarqube"
+    }
+  }
+}
+
+resource "null_resource" "sonarqube_helm_template" {
+  depends_on = ["null_resource.helm_init", "null_resource.fetch_sonarqube_helm"]
+
+  provisioner "local-exec" {
+    command = "helm template $${CHART} --namespace $${NAMESPACE} --name $${NAME} --values $${VALUES_FILE} --set ingress.hosts.0.name=$${HOST},postgresql.postgresServer=$${DATABASE_HOST},postgresql.service.port=$${DATABASE_PORT},postgresql.postgresDatabase=$${DATABASE_NAME},postgresql.postgresUser=$${DATABASE_USERNAME},postgresql.postgresPassword=$${DATABASE_PASSWORD} > $${KUSTOMISE_PATH}"
 
     environment = {
       KUBECONFIG = "${var.iks_cluster_config_file}"
-      CHART = "${path.module}/ibm-sonarqube"
+      CHART = "${path.module}/charts/sonarqube"
       NAME = "sonarqube"
       NAMESPACE = "${var.releases_namespace}"
       VALUES_FILE = "${path.module}/sonarqube-values.yaml"
@@ -111,10 +112,23 @@ resource "null_resource" "sonarqube_release" {
       DATABASE_NAME = "${var.sonarqube_postgresql_database_name}"
       DATABASE_USERNAME = "${var.sonarqube_postgresql_service_account_username}"
       DATABASE_PASSWORD = "${var.sonarqube_postgresql_service_account_password}"
+      KUSTOMISE_PATH = "${path.module}/sonarqube/base.yaml"
     }
   }
 }
 
+resource "null_resource" "sonarqube_release" {
+  depends_on = ["null_resource.sonarqube_helm_template"]
+
+  provisioner "local-exec" {
+    command = "kustomize build ${path.module}/sonarqube | kubectl apply -n $${NAMESPACE} -f -"
+
+    environment = {
+      KUBECONFIG = "${var.iks_cluster_config_file}"
+      NAMESPACE = "${var.releases_namespace}"
+    }
+  }
+}
 resource "null_resource" "catalystdashboard_release" {
   depends_on = ["null_resource.helm_init"]
 
