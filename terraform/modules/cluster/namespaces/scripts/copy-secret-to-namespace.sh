b19822024cb55c0ec0de2e1c6f002a5cf9324145
Sean Sundberg <seansund@us.ibm.com>
2019-08-16 14:28:56 -0500

    Adds logic to copy-secret-to-namespace.sh to test exact secret name match to see if it already exists

--- terraform/modules/cluster/namespaces/scripts/copy-secret-to-namespace.sh
+++ terraform/modules/cluster/namespaces/scripts/copy-secret-to-namespace.sh
@@ -35,7 +35,7 @@ if [[ $? -ne 0 ]]; then
   exit 0
 fi
 
-if [[ $(kubectl get secrets -n "${TO_NAMESPACE}" -o jsonpath='{ range .items[*] }{ .metadata.name }{ "\n" }{ end }' | grep ${SECRET_NAME} | wc -l | xargs) -eq 0 ]]; then
+if [[ $(kubectl get secrets -n "${TO_NAMESPACE}" -o jsonpath='{ range .items[*] }{ .metadata.name }{ "\n" }{ end }' | grep -E "^${SECRET_NAME}$" | wc -l | xargs) -eq 0 ]]; then
     echo "*** Copying ${SECRET_NAME} from ${FROM_NAMESPACE} namespace to ${TO_NAMESPACE} namespace"
 
     kubectl get secret ${SECRET_NAME} --namespace=${FROM_NAMESPACE} -oyaml | sed "s/namespace: ${FROM_NAMESPACE}/namespace: ${TO_NAMESPACE}/g" | kubectl apply --namespace=${TO_NAMESPACE} -f -
