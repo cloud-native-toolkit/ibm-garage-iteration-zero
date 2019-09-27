8328f42cebc59a68732faf2153791e2215b562c9
Sean Sundberg <seansund@us.ibm.com>
2019-09-27 11:37:48 -0500

    Updates version for dashboard module and adds input variable tools_config_maps

--- terraform/stages/stage2-catalystdashboard.tf
+++ terraform/stages/stage2-catalystdashboard.tf
@@ -1,11 +1,13 @@
 module "dev_tools_catalystdashboard_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//tools/catalystdashboard_release?ref=v1.0.0"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//tools/catalystdashboard_release?ref=v1.0.3"
 
   cluster_ingress_hostname = "${module.dev_cluster.ingress_hostname}"
   cluster_config_file      = "${module.dev_cluster.config_file_path}"
   cluster_type             = "${var.cluster_type}"
   releases_namespace       = "${module.dev_cluster_namespaces.tools_namespace_name}"
-  jenkins_secret_name      = "${module.dev_tools_jenkins_release.secret_name}"
-  sonarqube_secret_name    = "${module.dev_tools_sonarqube_release.secret_name}"
-  pactbroker_secret_name   = "${module.dev_tools_pactbroker_release.secret_name}"
+  tool_config_maps         = [
+    "${module.dev_tools_jenkins_release.secret_name}",
+    "${module.dev_tools_sonarqube_release.secret_name}",
+    "${module.dev_tools_pactbroker_release.secret_name}"
+  ]
 }
