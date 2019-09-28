0fa1bc28a7a15fe706e13abf4d524a8aef538e95
Sean Sundberg <seansund@us.ibm.com>
2019-09-27 23:02:47 -0500

    Updates module version for tools and logdna to v1.0.5

--- terraform/stages/stage2-jenkins.tf
+++ terraform/stages/stage2-jenkins.tf
@@ -1,5 +1,5 @@
 module "dev_tools_jenkins_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//tools/jenkins_release?ref=argocd-url"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//tools/jenkins_release?ref=v1.0.5"
 
   cluster_ingress_hostname = "${module.dev_cluster.ingress_hostname}"
   cluster_config_file      = "${module.dev_cluster.config_file_path}"
--- terraform/stages/stage3-logdna.tf
+++ terraform/stages/stage3-logdna.tf
@@ -1,5 +1,5 @@
 module "dev_serviceaccount_logdna-agent" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cluster/serviceaccount?ref=argocd-url"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cluster/serviceaccount?ref=v1.0.5"
 
   cluster_type             = "${var.cluster_type}"
   cluster_config_file_path = "${module.dev_cluster.config_file_path}"
@@ -9,7 +9,7 @@ module "dev_serviceaccount_logdna-agent" {
 }
 
 module "dev_infrastructure_logdna" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//infrastructure/logdna?ref=argocd-url"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//infrastructure/logdna?ref=v1.0.5"
 
   resource_group_name      = "${module.dev_cluster.resource_group_name}"
   resource_location        = "${module.dev_cluster.region}"
