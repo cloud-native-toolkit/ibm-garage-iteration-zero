2ce138dd94af9e3656a1e6c2a9443294e9188223
Sean Sundberg <seansund@us.ibm.com>
2019-10-16 12:22:28 -0500

    Fixes typo in postgres stage where tools namespace was mapped to the wrong variable

--- terraform/stages/stage1-postgres.tf
+++ terraform/stages/stage1-postgres.tf
@@ -5,7 +5,7 @@ module "dev_infrastructure_postgres" {
   resource_location   = "${var.vlan_region}"
   server_exists       = "${var.postgres_server_exists}"
   cluster_id          = "${module.dev_cluster.id}"
-  tools_namespace     = "${module.dev_cluster_namespaces.dev_namespace_name}"
+  tools_namespace     = "${module.dev_cluster_namespaces.tools_namespace_name}"
   dev_namespace       = "${module.dev_cluster_namespaces.dev_namespace_name}"
   test_namespace      = "${module.dev_cluster_namespaces.test_namespace_name}"
   staging_namespace   = "${module.dev_cluster_namespaces.staging_namespace_name}"
