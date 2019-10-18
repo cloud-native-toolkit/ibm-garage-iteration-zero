8b50219ce5cf4a564ee3009ff004e5b0258750fb
Sean Sundberg <seansund@us.ibm.com>
2019-10-18 11:30:04 -0500

    Updates cluster stage to use 'dev' version of the module

--- terraform/stages/stage1-cluster.tf
+++ terraform/stages/stage1-cluster.tf
@@ -1,5 +1,5 @@
 module "dev_cluster" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/cluster/ibmcloud?ref=v2.0.0"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/cluster/ibmcloud?ref=dev"
 
   resource_group_name     = "${var.resource_group_name}"
   cluster_name            = "${var.cluster_name}"
