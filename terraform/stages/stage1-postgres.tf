42c62568ab2fc002af0abaac639c8bad35251be6
Sean Sundberg <seansund@us.ibm.com>
2019-12-03 10:28:50 -0600

    Updates postgres version to v2.1.2 to pick up fix to make service name lower case

--- terraform/stages/stage1-postgres.tf
+++ terraform/stages/stage1-postgres.tf
@@ -1,5 +1,5 @@
 module "dev_infrastructure_postgres" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/postgres?ref=v2.1.0"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/postgres?ref=v2.1.2"
 
   resource_group_name = "${var.resource_group_name}"
   resource_location   = "${var.vlan_region}"
