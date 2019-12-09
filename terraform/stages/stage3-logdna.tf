3a71930c39b2569c685052bee1dcf30c12522a5d
Sean Sundberg <seansund@us.ibm.com>
2019-12-09 12:46:40 -0600

    Bumps LogDNA module version to v2.1.4 to correct issue

--- terraform/stages/stage3-logdna.tf
+++ terraform/stages/stage3-logdna.tf
@@ -9,7 +9,7 @@ module "dev_serviceaccount_logdna-agent" {
 }
 
 module "dev_infrastructure_logdna" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/logdna?ref=v2.1.0"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/logdna?ref=v2.1.4"
 
   resource_group_name      = "${module.dev_cluster.resource_group_name}"
   resource_location        = "${module.dev_cluster.region}"
