9b701b9aad188a9cbe04ac978e3438a31f7d2513
Sean Sundberg <seansund@us.ibm.com>
2020-01-28 23:06:01 +0100

    Adds stages-ocp4 directory and updates runTerraform.sh to apply it when cluster_type is ocp4

---
+++ terraform/stages-ocp4/stage2-sonarqube.tf
@@ -0,0 +1,25 @@
+module "dev_serviceaccount_sonarqube" {
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=ocp43"
+
+  cluster_type             = "${var.cluster_type}"
+  namespace                = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  cluster_config_file_path = "${module.dev_cluster.config_file_path}"
+  service_account_name     = "sonarqube"
+  sscs                     = ["anyuid", "privileged"]
+}
+
+module "dev_tools_sonarqube_release" {
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/sonarqube_release?ref=ocp43"
+
+  cluster_type             = "${var.cluster_type}"
+  cluster_ingress_hostname = "${module.dev_cluster.ingress_hostname}"
+  cluster_config_file      = "${module.dev_cluster.config_file_path}"
+  postgresql_username      = "${module.dev_infrastructure_postgres.postgresql_service_account_username}"
+  postgresql_password      = "${module.dev_infrastructure_postgres.postgresql_service_account_password}"
+  postgresql_hostname      = "${module.dev_infrastructure_postgres.postgresql_hostname}"
+  postgresql_port          = "${module.dev_infrastructure_postgres.postgresql_port}"
+  postgresql_database_name = "${module.dev_infrastructure_postgres.postgresql_database_name}"
+  releases_namespace       = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  service_account_name     = "${module.dev_serviceaccount_sonarqube.name}"
+  tls_secret_name          = "${module.dev_cluster.tls_secret_name}"
+}
