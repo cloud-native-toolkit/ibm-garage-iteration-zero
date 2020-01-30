3f79fe0d2efebc0e712a8d45c3d2614acf8043cc
Bobby Woolf <bwoolf@us.ibm.com>
2020-01-30 10:38:58 -0500

    Bump stage1-cluster.tf to module v2.1.8.1 to support ocp3 and ocp4

--- terraform/stages/stage1-cluster.tf
+++ terraform/stages/stage1-cluster.tf
@@ -1,5 +1,6 @@
 module "dev_cluster" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/cluster/ibmcloud?ref=v2.1.0"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/cluster/ibmcloud?ref=v2.1.8.1"
+
 
   resource_group_name     = "${var.resource_group_name}"
   cluster_name            = "${var.cluster_name}"
