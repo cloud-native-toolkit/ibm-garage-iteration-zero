ac62c300e2f8a5baeeb5c425e1ed214ea05b42af
Sean Sundberg <seansund@us.ibm.com>
2020-02-12 09:22:50 -0600

    Updates stages for terraform 0.12

--- terraform/stages/stage1-postgres.tf
+++ terraform/stages/stage1-postgres.tf
@@ -1,13 +1,13 @@
 module "dev_infrastructure_postgres" {
   source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/postgres?ref=v2.1.8"
 
-  resource_group_name = "${var.resource_group_name}"
-  resource_location   = "${var.vlan_region}"
-  server_exists       = "${var.postgres_server_exists}"
-  cluster_id          = "${module.dev_cluster.id}"
-  tools_namespace     = "${module.dev_cluster_namespaces.tools_namespace_name}"
-  dev_namespace       = "${module.dev_cluster_namespaces.dev_namespace_name}"
-  test_namespace      = "${module.dev_cluster_namespaces.test_namespace_name}"
-  staging_namespace   = "${module.dev_cluster_namespaces.staging_namespace_name}"
-  name_prefix         = "${var.name_prefix}"
+  resource_group_name = var.resource_group_name
+  resource_location   = var.vlan_region
+  server_exists       = var.postgres_server_exists
+  cluster_id          = module.dev_cluster.id
+  tools_namespace     = module.dev_cluster_namespaces.tools_namespace_name
+  dev_namespace       = module.dev_cluster_namespaces.dev_namespace_name
+  test_namespace      = module.dev_cluster_namespaces.test_namespace_name
+  staging_namespace   = module.dev_cluster_namespaces.staging_namespace_name
+  name_prefix         = var.name_prefix
 }
--- terraform/stages/stage3-logdna.tf
+++ terraform/stages/stage3-logdna.tf
@@ -1,9 +1,9 @@
 module "dev_serviceaccount_logdna-agent" {
   source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=ocp43"
 
-  cluster_type             = "${var.cluster_type}"
-  cluster_config_file_path = "${module.dev_cluster.config_file_path}"
-  namespace                = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  cluster_type             = var.cluster_type
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  namespace                = module.dev_cluster_namespaces.tools_namespace_name
   service_account_name     = "logdna-agent"
   sscs                     = ["privileged"]
 }
@@ -11,11 +11,11 @@ module "dev_serviceaccount_logdna-agent" {
 module "dev_infrastructure_logdna" {
   source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/logdna?ref=ocp43"
 
-  resource_group_name      = "${module.dev_cluster.resource_group_name}"
-  resource_location        = "${module.dev_cluster.region}"
-  cluster_config_file_path = "${module.dev_cluster.config_file_path}"
-  cluster_type             = "${var.cluster_type}"
-  service_account_name     = "${module.dev_serviceaccount_logdna-agent.name}"
-  name_prefix              = "${var.name_prefix}"
-  namespace                = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  resource_group_name      = module.dev_cluster.resource_group_name
+  resource_location        = module.dev_cluster.region
+  cluster_type             = var.cluster_type
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  service_account_name     = module.dev_serviceaccount_logdna-agent.name
+  name_prefix              = var.name_prefix
+  namespace                = module.dev_cluster_namespaces.tools_namespace_name
 }
