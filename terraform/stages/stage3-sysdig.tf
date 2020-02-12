c01f3661cd33dd50597e29001e80fb80e12decf7
Sean Sundberg <seansund@us.ibm.com>
2020-02-12 16:22:38 -0600

    Converts service provision back to terraform provider instead of operator

--- terraform/stages/stage3-sysdig.tf
+++ terraform/stages/stage3-sysdig.tf
@@ -1,11 +1,12 @@
 module "dev_infrastructure_sysdig" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/sysdig?ref=v2.1.8"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/sysdig?ref=v2.1.8"
 
   resource_group_name      = "${module.dev_cluster.resource_group_name}"
   resource_location        = "${module.dev_cluster.region}"
   cluster_config_file_path = "${module.dev_cluster.config_file_path}"
-  tools_namespace          = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  dev_namespace            = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  test_namespace           = "${module.dev_cluster_namespaces.tools_namespace_name}"
+  staging_namespace        = "${module.dev_cluster_namespaces.tools_namespace_name}"
   cluster_type             = "${var.cluster_type}"
   name_prefix              = "${var.name_prefix}"
-  service_namespace        = "${module.dev_software_cloud_operator.namespace}"
 }
