7c27647579a19f8796d5ec2281179765422fbb43
Sean Sundberg <seansund@us.ibm.com>
2020-02-12 16:22:38 -0600

    Converts service provision back to terraform provider instead of operator

--- terraform/stages/stage3-cloudant.tf
+++ terraform/stages/stage3-cloudant.tf
@@ -1,5 +1,5 @@
 module "dev_infrastructure_cloudant" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/cloudant?ref=v2.1.8"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/cloudant?ref=v2.1.8"
 
   resource_group_name = "${module.dev_cluster.resource_group_name}"
   resource_location   = "${module.dev_cluster.region}"
@@ -8,6 +8,4 @@ module "dev_infrastructure_cloudant" {
   test_namespace      = "${module.dev_cluster_namespaces.test_namespace_name}"
   staging_namespace   = "${module.dev_cluster_namespaces.staging_namespace_name}"
   name_prefix         = "${var.name_prefix}"
-  service_namespace   = "${module.dev_software_cloud_operator.namespace}"
-  cluster_config_file = "${module.dev_cluster.config_file_path}"
 }
--- terraform/stages/stage3-logdna.tf
+++ terraform/stages/stage3-logdna.tf
@@ -9,14 +9,13 @@ module "dev_serviceaccount_logdna-agent" {
 }
 
 module "dev_infrastructure_logdna" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/operator-services/logdna?ref=v2.1.8"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/logdna?ref=v2.1.8"
 
   resource_group_name      = "${module.dev_cluster.resource_group_name}"
   resource_location        = "${module.dev_cluster.region}"
-  cluster_type             = "${var.cluster_type}"
   cluster_config_file_path = "${module.dev_cluster.config_file_path}"
+  cluster_type             = "${var.cluster_type}"
   service_account_name     = "${module.dev_serviceaccount_logdna-agent.name}"
   name_prefix              = "${var.name_prefix}"
   namespace                = "${module.dev_cluster_namespaces.tools_namespace_name}"
-  service_namespace        = "${module.dev_software_cloud_operator.namespace}"
 }
