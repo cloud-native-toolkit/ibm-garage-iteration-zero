72d27360316c30a5609d84918963bcf3bc758703
Larry Steck <lsteck@us.ibm.com>
2020-02-14 16:36:26 -0600

    pass cluster_version to dashboard

--- terraform/stages-ocp4/stage2-catalystdashboard.tf
+++ terraform/stages-ocp4/stage2-catalystdashboard.tf
@@ -4,6 +4,7 @@ module "dev_tools_catalystdashboard_release" {
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   cluster_type             = var.cluster_type
+  cluster_version          = module.dev_cluster.cluster_version
   releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
   tls_secret_name          = module.dev_cluster.tls_secret_name
 }
