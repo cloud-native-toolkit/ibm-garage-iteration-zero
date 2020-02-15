a476b3fc1c5222e40168f73d6de10355b5fe8aaf
Sean Sundberg <seansund@us.ibm.com>
2020-02-15 15:34:57 -0600

    Updates to stages from ocp4.3 testing and iks and ocp3 regression testing

--- terraform/stages/stage1-postgres.tf
+++ terraform/stages/stage1-postgres.tf
@@ -5,9 +5,13 @@ module "dev_infrastructure_postgres" {
   resource_location   = var.vlan_region
   server_exists       = var.postgres_server_exists
   cluster_id          = module.dev_cluster.id
-  tools_namespace     = module.dev_cluster_namespaces.tools_namespace_name
-  dev_namespace       = module.dev_cluster_namespaces.dev_namespace_name
-  test_namespace      = module.dev_cluster_namespaces.test_namespace_name
-  staging_namespace   = module.dev_cluster_namespaces.staging_namespace_name
+  namespaces          = [
+    module.dev_cluster_namespaces.tools_namespace_name,
+    module.dev_cluster_namespaces.dev_namespace_name,
+    module.dev_cluster_namespaces.test_namespace_name,
+    module.dev_cluster_namespaces.staging_namespace_name
+  ]
+  namespace_count     = 4
   name_prefix         = var.name_prefix
+  tags                = [module.dev_cluster.tag]
 }
--- terraform/stages/stage3-cloud_object_storage.tf
+++ terraform/stages/stage3-cloud_object_storage.tf
@@ -4,8 +4,10 @@ module "dev_infrastructure_cos" {
   resource_group_name = module.dev_cluster.resource_group_name
   resource_location   = module.dev_cluster.region
   cluster_id          = module.dev_cluster.id
-  dev_namespace       = module.dev_cluster_namespaces.dev_namespace_name
-  test_namespace      = module.dev_cluster_namespaces.test_namespace_name
-  staging_namespace   = module.dev_cluster_namespaces.staging_namespace_name
+  namespaces          = [
+    module.dev_cluster_namespaces.dev_namespace_name,
+    module.dev_cluster_namespaces.test_namespace_name,
+    module.dev_cluster_namespaces.staging_namespace_name
+  ]
   name_prefix         = var.name_prefix
 }
