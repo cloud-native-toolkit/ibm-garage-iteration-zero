ee307691856c4aba4f71a7a5f2441c45fc61cda5
Sean Sundberg <seansund@us.ibm.com>
2020-02-20 22:56:24 -0800

    Updates handling of namespaces

--- terraform/stages/stage1-postgres.tf
+++ terraform/stages/stage1-postgres.tf
@@ -5,13 +5,8 @@ module "dev_infrastructure_postgres" {
   resource_location   = var.vlan_region
   server_exists       = var.postgres_server_exists
   cluster_id          = module.dev_cluster.id
-  namespaces          = [
-    module.dev_cluster_namespaces.tools_namespace_name,
-    module.dev_cluster_namespaces.dev_namespace_name,
-    module.dev_cluster_namespaces.test_namespace_name,
-    module.dev_cluster_namespaces.staging_namespace_name
-  ]
-  namespace_count     = 4
+  namespaces          = concat([module.dev_cluster_namespaces.tools_namespace_name], module.dev_cluster_namespaces.other_namespaces)
+  namespace_count     = var.other_namespace_count+1
   name_prefix         = var.name_prefix
   tags                = [module.dev_cluster.tag]
 }
--- terraform/stages/stage3-appid.tf
+++ terraform/stages/stage3-appid.tf
@@ -4,13 +4,8 @@ module "dev_infrastructure_appid" {
   resource_group_name = module.dev_cluster.resource_group_name
   resource_location   = module.dev_cluster.region
   cluster_id          = module.dev_cluster.id
-  namespaces          = [
-    module.dev_cluster_namespaces.tools_namespace_name,
-    module.dev_cluster_namespaces.dev_namespace_name,
-    module.dev_cluster_namespaces.test_namespace_name,
-    module.dev_cluster_namespaces.staging_namespace_name
-  ]
-  namespace_count     = 4
+  namespaces          = concat([module.dev_cluster_namespaces.tools_namespace_name], module.dev_cluster_namespaces.other_namespaces)
+  namespace_count     = var.other_namespace_count+1
   name_prefix         = var.name_prefix
   tags                = [module.dev_cluster.tag]
 }
