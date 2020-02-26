84114a2b7124684f44d1f00aec63593cfbe385cc
Sean Sundberg <seansund@us.ibm.com>
2020-02-26 11:40:48 -0600

    Updates config (temporarily) for ocp 4.3

--- terraform/stages-ocp4/stage1-namespaces.tf
+++ terraform/stages-ocp4/stage1-namespaces.tf
@@ -1,9 +1,19 @@
 module "dev_cluster_namespaces" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/namespaces?ref=v2.2.1"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/namespaces?ref=ocp43-lite"
 
   cluster_type             = module.dev_cluster.type
   cluster_config_file_path = module.dev_cluster.config_file_path
   tls_secret_name          = module.dev_cluster.tls_secret_name
   tools_namespace          = var.tools_namespace
   release_namespaces       = var.release_namespaces
+}
+
+module "dev_sre_namespace" {
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/namespaces?ref=ocp43-lite"
+
+  cluster_type             = module.dev_cluster.type
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  tls_secret_name          = module.dev_cluster.tls_secret_name
+  tools_namespace          = var.sre_namespace
+  release_namespaces       = []
 }
