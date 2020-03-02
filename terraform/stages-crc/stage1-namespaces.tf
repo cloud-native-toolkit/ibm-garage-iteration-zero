0767781145401333889e6520168d20f288b1cd87
Sean Sundberg <seansund@us.ibm.com>
2020-03-02 14:24:41 -0600

    Updates stages for CRC

--- terraform/stages-crc/stage1-namespaces.tf
+++ terraform/stages-crc/stage1-namespaces.tf
@@ -1,11 +1,9 @@
 module "dev_cluster_namespaces" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/namespaces?ref=v2.0.7"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/namespaces?ref=ocp43-lite"
 
-  cluster_type             = "${module.dev_cluster.type}"
-  cluster_config_file_path = "${module.dev_cluster.config_file_path}"
-  tls_secret_name          = "${module.dev_cluster.tls_secret_name}"
-  tools_namespace          = "${var.tools_namespace}"
-  dev_namespace            = "${var.dev_namespace}"
-  test_namespace           = "${var.test_namespace}"
-  staging_namespace        = "${var.staging_namespace}"
+  cluster_type             = module.dev_cluster.type
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  tls_secret_name          = module.dev_cluster.tls_secret_name
+  tools_namespace          = var.tools_namespace
+  release_namespaces       = var.release_namespaces
 }
