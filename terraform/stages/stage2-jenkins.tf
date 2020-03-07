a331710663c8916976774337cc716e075d179a88
Sean Sundberg <seansund@us.ibm.com>
2020-03-06 22:51:31 -0600

    Updates Jenkins module to pass ci namespace. Updates postgres and sonarqube modules for crc

--- terraform/stages/stage2-jenkins.tf
+++ terraform/stages/stage2-jenkins.tf
@@ -4,7 +4,8 @@ module "dev_tools_jenkins_release" {
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   cluster_type             = var.cluster_type
-  releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
+  tools_namespace          = module.dev_cluster_namespaces.tools_namespace_name
+  ci_namespace             = module.dev_cluster_namespaces.release_namespaces[0]
   tls_secret_name          = module.dev_cluster.tls_secret_name
   server_url               = module.dev_cluster.server_url
 }
