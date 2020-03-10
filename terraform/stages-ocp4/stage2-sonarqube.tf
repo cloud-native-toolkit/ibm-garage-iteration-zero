0118bbceaddd1be8a85b7ee407ec8a2dc348e084
Carlos Santana <csantana23@gmail.com>
2020-03-10 13:55:43 -0400

    move postgresql into sonarqube

--- terraform/stages-ocp4/stage2-sonarqube.tf
+++ terraform/stages-ocp4/stage2-sonarqube.tf
@@ -1,3 +1,16 @@
+module "dev_infrastructure_postgres" {
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//self-managed/software/postgres?ref=ocp43-lite"
+
+  resource_group_name = var.resource_group_name
+  resource_location   = var.vlan_region
+  server_exists       = var.postgres_server_exists
+  cluster_id          = module.dev_cluster.id
+  namespaces          = concat([module.dev_cluster_namespaces.tools_namespace_name], module.dev_cluster_namespaces.release_namespaces)
+  namespace_count     = var.release_namespace_count+1
+  postgresql_database = "sonarqube-psql"
+  cluster_type        = var.cluster_type
+}
+
 module "dev_serviceaccount_sonarqube" {
   source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=ocp43-lite"
 
