7661d7a65a3b66504ba95569ac12f547c3db27c2
Sean Sundberg <seansund@us.ibm.com>
2020-03-14 10:57:49 -0500

    Updates swagger module version to v2.2.3

--- terraform/stages/stage2-swagger.tf
+++ terraform/stages/stage2-swagger.tf
@@ -1,5 +1,5 @@
 module "dev_tools_swagger_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/swagger_editor?ref=v2.2.2"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/swagger_editor?ref=v2.2.3"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
