b4816c02f2b2365f491d52f8afc91bb577b42421
Sean Sundberg <seansund@us.ibm.com>
2020-03-14 14:43:56 -0500

    Updates version of postgres module to v2.2.4 for IKS and OCP 3 installs

--- terraform/stages/stage1-postgres.tf
+++ terraform/stages/stage1-postgres.tf
@@ -1,5 +1,5 @@
 module "dev_infrastructure_postgres" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/postgres?ref=ocp43-postgres"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//cloud-managed/services/postgres?ref=ref=v2.2.4"
 
   resource_group_name = var.resource_group_name
   resource_location   = var.vlan_region
