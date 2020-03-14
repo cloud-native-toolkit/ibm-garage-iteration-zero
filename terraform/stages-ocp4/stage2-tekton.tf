bfa26eabaed0499e748b26b918aa4640d3d0b8c2
Sean Sundberg <seansund@us.ibm.com>
2020-03-14 15:05:01 -0500

    Bumpts tekton module version to v2.2.5

--- terraform/stages-ocp4/stage2-tekton.tf
+++ terraform/stages-ocp4/stage2-tekton.tf
@@ -1,5 +1,5 @@
 module "dev_tools_tekton_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/tekton_release?ref=v2.2.2"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/tekton_release?ref=v2.2.5"
 
   cluster_type             = module.dev_cluster.type
   cluster_config_file_path = module.dev_cluster.config_file_path
@@ -8,7 +8,7 @@ module "dev_tools_tekton_release" {
 }
 
 module "dev_tools_tekton_resources" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/tekton_resources?ref=v2.2.2"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/tekton_resources?ref=v2.2.5"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
   tekton_namespace         = module.dev_tools_tekton_release.namespace
