38e604e110c5c3f2eb687a83c0562e0f0c3d3df3
Sean Sundberg <seansund@us.ibm.com>
2020-03-15 22:36:37 -0500

    Bumps argocd module to v2.2.9 to fix IKS issue

--- terraform/stages-crc/stage2-argocd.tf
+++ terraform/stages-crc/stage2-argocd.tf
@@ -1,5 +1,5 @@
 module "dev_tools_argocd_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/argocd_release?ref=v2.2.2"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/argocd_release?ref=v2.2.9"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
