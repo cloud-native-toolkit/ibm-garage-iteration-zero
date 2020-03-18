53fe748e12ca4d1f08436c82c32ade8c9386d26f
Sean Sundberg <seansund@us.ibm.com>
2020-03-18 01:48:08 -0500

    Bumps sonarqube version to 2.2.13 to support inline postgres

--- terraform/stages-crc/stage2-sonarqube.tf
+++ terraform/stages-crc/stage2-sonarqube.tf
@@ -1,17 +1,5 @@
-module "dev_infrastructure_postgres" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//self-managed/software/postgres?ref=v2.2.7"
-
-  resource_group_name = var.resource_group_name
-  resource_location   = var.vlan_region
-  server_exists       = var.postgres_server_exists
-  cluster_id          = module.dev_cluster.id
-  namespaces          = concat([module.dev_cluster_namespaces.tools_namespace_name], module.dev_cluster_namespaces.release_namespaces)
-  namespace_count     = var.release_namespace_count+1
-  cluster_type        = var.cluster_type
-}
-
 module "dev_serviceaccount_sonarqube" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=v2.2.7"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=v2.2.8"
 
   cluster_type             = var.cluster_type
   namespace                = module.dev_cluster_namespaces.tools_namespace_name
@@ -21,18 +9,12 @@ module "dev_serviceaccount_sonarqube" {
 }
 
 module "dev_tools_sonarqube_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/sonarqube_release?ref=v2.2.7"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/sonarqube_release?ref=v2.2.13"
 
   cluster_type             = var.cluster_type
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
-  postgresql_username      = module.dev_infrastructure_postgres.postgresql_service_account_username
-  postgresql_password      = module.dev_infrastructure_postgres.postgresql_service_account_password
-  postgresql_hostname      = module.dev_infrastructure_postgres.postgresql_hostname
-  postgresql_port          = module.dev_infrastructure_postgres.postgresql_port
-  postgresql_database_name = module.dev_infrastructure_postgres.postgresql_database_name
   releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
   service_account_name     = module.dev_serviceaccount_sonarqube.name
   tls_secret_name          = module.dev_cluster.tls_secret_name
-  storage_class            = ""
 }
--- terraform/stages/stage2-sonarqube.tf
+++ terraform/stages/stage2-sonarqube.tf
@@ -1,5 +1,5 @@
 module "dev_serviceaccount_sonarqube" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=v2.2.7"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/cluster/serviceaccount?ref=v2.2.8"
 
   cluster_type             = var.cluster_type
   namespace                = module.dev_cluster_namespaces.tools_namespace_name
@@ -9,16 +9,11 @@ module "dev_serviceaccount_sonarqube" {
 }
 
 module "dev_tools_sonarqube_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/sonarqube_release?ref=v2.2.7"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/sonarqube_release?ref=v2.2.13"
 
   cluster_type             = var.cluster_type
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
-  postgresql_username      = module.dev_infrastructure_postgres.postgresql_service_account_username
-  postgresql_password      = module.dev_infrastructure_postgres.postgresql_service_account_password
-  postgresql_hostname      = module.dev_infrastructure_postgres.postgresql_hostname
-  postgresql_port          = module.dev_infrastructure_postgres.postgresql_port
-  postgresql_database_name = module.dev_infrastructure_postgres.postgresql_database_name
   releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
   service_account_name     = module.dev_serviceaccount_sonarqube.name
   tls_secret_name          = module.dev_cluster.tls_secret_name
