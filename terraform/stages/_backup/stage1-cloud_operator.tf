eb65ea3d75f77b73c758b59ddefce50860dc94a1
Sean Sundberg <seansund@us.ibm.com>
2020-04-01 22:37:37 -0500

    Updates stages/ to simplify setup
    - Removes stages-ocp4 and updates runTerraform to pull from stages/
    - Renames combines _tmp and _backup into catalog directory
    - Updates version numbers for tekton modules to only install on ocp4

--- terraform/stages/_backup/stage1-cloud_operator.tf
+++
@@ -1,8 +0,0 @@
-module "dev_software_cloud_operator" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//self-managed/software/cloud_operator?ref=v2.2.0"
-
-  resource_group_name = var.resource_group_name
-  resource_location   = var.vlan_region
-  cluster_config_file = module.dev_cluster.config_file_path
-  ibmcloud_api_key    = module.dev_cluster.ibmcloud_api_key
-}
