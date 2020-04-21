8e89a447ea7a5372d946a7378d221e890313f40d
Sean Sundberg <seansund@users.noreply.github.com>
2020-04-20 22:11:10 -0500

    Bumps developer dashboard module to v2.4.0 (#56)

--- terraform/stages/stage2-dashboard.tf
+++ terraform/stages/stage2-dashboard.tf
@@ -1,5 +1,5 @@
 module "dev_tools_dashboard_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/developerdashboard_release?ref=v2.2.11"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/developerdashboard_release?ref=v2.4.0"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
