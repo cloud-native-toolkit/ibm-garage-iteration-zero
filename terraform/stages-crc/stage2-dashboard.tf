4f5454c2f9b4ecbda16a9c8b6f6ee43392b132ad
Sean Sundberg <seansund@us.ibm.com>
2020-04-23 23:48:35 -0500

    Bumps dashboard module to v2.4.4 and adds image_tag variable (#64)

--- terraform/stages-crc/stage2-dashboard.tf
+++ terraform/stages-crc/stage2-dashboard.tf
@@ -1,9 +1,10 @@
 module "dev_tools_dashboard_release" {
-  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/developerdashboard_release?ref=v2.4.0"
+  source = "github.com/ibm-garage-cloud/garage-terraform-modules.git//generic/tools/developerdashboard_release?ref=v2.4.4"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   cluster_type             = var.cluster_type
   releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
   tls_secret_name          = module.dev_cluster.tls_secret_name
+  image_tag                = "1.0.31"
 }
