30c33bb0fea067715767c72386aef29c0ddb2662
Sean Sundberg <seansund@us.ibm.com>
2020-06-20 20:27:16 -0500

    Updates stage2-pactbroker to v1.3.0

--- terraform/stages/stage2-pactbroker.tf
+++ terraform/stages/stage2-pactbroker.tf
@@ -1,5 +1,5 @@
 module "dev_tools_pactbroker_release" {
-  source = "github.com/ibm-garage-cloud/terraform-tools-pactbroker.git?ref=v1.2.0"
+  source = "github.com/ibm-garage-cloud/terraform-tools-pactbroker.git?ref=v1.3.0"
 
   cluster_ingress_hostname                      = module.dev_cluster.ingress_hostname
   cluster_config_file                           = module.dev_cluster.config_file_path
