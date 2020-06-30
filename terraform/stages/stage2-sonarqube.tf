d31e76adcbd65e584e415c5b75e2fa453f6fa8e9
Sean Sundberg <seansund@us.ibm.com>
2020-06-30 17:28:31 -0500

    Bumps sonarqube to v1.5.1 (#91)

--- terraform/stages/stage2-sonarqube.tf
+++ terraform/stages/stage2-sonarqube.tf
@@ -1,7 +1,7 @@
 module "dev_tools_sonarqube" {
-  source = "github.com/ibm-garage-cloud/terraform-tools-sonarqube.git?ref=v1.4.0"
+  source = "github.com/ibm-garage-cloud/terraform-tools-sonarqube.git?ref=v1.5.1"
 
-  cluster_type             = var.cluster_type
+  cluster_type             = module.dev_cluster.type_code
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
@@ -15,14 +15,4 @@ module "dev_tools_sonarqube" {
     port          = ""
     database_name = ""
   }
-}
-
-module "dev_serviceaccount_sonarqube" {
-  source = "github.com/ibm-garage-cloud/terraform-cluster-serviceaccount.git?ref=v1.3.0"
-
-  cluster_type             = var.cluster_type
-  cluster_config_file_path = module.dev_cluster.config_file_path
-  namespace                = module.dev_tools_sonarqube.namespace
-  service_account_name     = module.dev_tools_sonarqube.service_account
-  sscs                     = ["anyuid", "privileged"]
 }
