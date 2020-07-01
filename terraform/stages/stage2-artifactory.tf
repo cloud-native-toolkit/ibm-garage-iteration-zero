da3582e8141934bbe1d51e2211354e23943e6ab7
Sean Sundberg <seansund@us.ibm.com>
2020-06-30 23:16:24 -0500

    Bumps artifactory, jenkins, and sysdig

--- terraform/stages/stage2-artifactory.tf
+++ terraform/stages/stage2-artifactory.tf
@@ -1,19 +1,9 @@
-module "dev_serviceaccount_artifactory" {
-  source = "github.com/ibm-garage-cloud/terraform-cluster-serviceaccount.git?ref=v1.2.0"
-
-  cluster_type             = var.cluster_type
-  namespace                = module.dev_cluster_namespaces.tools_namespace_name
-  cluster_config_file_path = module.dev_cluster.config_file_path
-  service_account_name     = "artifactory-artifactory"
-  sscs                     = ["anyuid", "privileged"]
-}
-
 module "dev_tools_artifactory" {
-  source = "github.com/ibm-garage-cloud/terraform-tools-artifactory.git?ref=v1.2.0"
+  source = "github.com/ibm-garage-cloud/terraform-tools-artifactory.git?ref=v1.4.0"
 
-  cluster_type             = var.cluster_type
-  service_account          = module.dev_serviceaccount_artifactory.name
-  releases_namespace       = module.dev_serviceaccount_artifactory.namespace
+  cluster_type             = module.dev_cluster.type_code
+  service_account          = "artifactory-artifactory"
+  releases_namespace       = module.dev_cluster_namespaces.tools_namespace_name
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   tls_secret_name          = module.dev_cluster.tls_secret_name
