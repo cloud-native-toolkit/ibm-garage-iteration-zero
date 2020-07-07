9e88f8cc41f5b2fd8d6d4fe03b0649e83045a892
Sean Sundberg <seansund@us.ibm.com>
2020-07-07 15:30:54 -0500

    Removes tekton-dashboard namespace

--- terraform/stages/stage2-tekton.tf
+++ terraform/stages/stage2-tekton.tf
@@ -1,12 +1,3 @@
-module "dev_tekton_dashboard_namespace" {
-  source = "github.com/ibm-garage-cloud/terraform-k8s-namespace.git?ref=v2.2.0"
-
-  cluster_type             = module.dev_cluster.type_code
-  cluster_config_file_path = module.dev_cluster.config_file_path
-  tls_secret_name          = module.dev_cluster.tls_secret_name
-  name                     = "openshift-pipelines"
-}
-
 module "dev_tools_tekton_release" {
   source = "github.com/ibm-garage-cloud/terraform-tools-tekton.git?ref=v1.3.3"
 
@@ -14,7 +5,6 @@ module "dev_tools_tekton_release" {
   cluster_config_file_path   = module.dev_cluster.config_file_path
   cluster_ingress_hostname   = module.dev_cluster.ingress_hostname
   tools_namespace            = module.dev_tools_namespace.name
-  tekton_dashboard_namespace = module.dev_tekton_dashboard_namespace.name
 }
 
 module "dev_tools_tekton_resources" {
