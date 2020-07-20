b6c59fd7c58d763090677cc6bc35e00b68298675
Sean Sundberg <seansund@us.ibm.com>
2020-07-20 00:57:48 -0500

    Adds support for multiple tile configurations

---
+++ .tile/tiles/sre-tools/stages/stage.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "> 0.12.0"
+}
+
+module "sre_logdna" {
+  source = "github.com/ibm-garage-cloud/terraform-ibm-logdna.git?ref=multi-mode"
+
+  resource_group_name      = var.resource_group_name
+  resource_location        = var.region
+  name_prefix              = var.name_prefix
+  provision                = var.provision_logdna == "true"
+  name                     = var.logdna_name
+}
+
+module "sre_sysdig" {
+  source = "github.com/ibm-garage-cloud/terraform-ibm-sysdig.git?ref=multi-mode"
+
+  resource_group_name      = var.resource_group_name
+  resource_location        = var.region
+  name_prefix              = var.name_prefix
+  provision                = var.provision_sysdig == "true"
+  name                     = var.sysdig_name
+}
+
+module "sre_activity-tracker" {
+  source = "github.com/ibm-garage-cloud/terraform-ibm-activity-tracker.git?ref=v1.1.1"
+
+  resource_group_name      = var.resource_group_name
+  resource_location        = var.region
+  name_prefix              = var.name_prefix
+  provision                = var.provision_activity_tracker == "true"
+}
