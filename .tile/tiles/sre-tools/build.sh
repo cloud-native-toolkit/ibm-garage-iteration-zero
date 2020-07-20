7c36fbac057e89f3de729f30ea6f8a8b144823db
Sean Sundberg <seansund@us.ibm.com>
2020-07-20 09:39:05 -0500

    Updates cloudnative-toolkit to include logdna, sysdig, and activity-tracker modules

--- .tile/tiles/sre-tools/build.sh
+++ .tile/tiles/sre-tools/build.sh
@@ -33,15 +33,6 @@ mkdir -p "${WORKSPACE_DIR}"
 
 SRC_DIR="./terraform"
 
-ENVIRONMENT_TFVARS="${SRC_DIR}/settings/environment.tfvars"
-TFVARS="${WORKSPACE_DIR}/terraform.tfvars"
-cat "${ENVIRONMENT_TFVARS}" > "${TFVARS}"
-
-# Read terraform.tfvars to see if cluster_exists, postgres_server_exists, and cluster_type are set
-# If not, get them from the user and write them to a file
-
-CLUSTER_MANAGEMENT="ibmcloud"
-CLUSTER_TYPE="openshift"
 STAGES_DIRECTORY="stages"
 
 cp "${SCRIPT_DIR}/${STAGES_DIRECTORY}/variables.tf" "${WORKSPACE_DIR}"
--- terraform/stages/stage3-sysdig.tf
+++ terraform/stages/stage3-sysdig.tf
@@ -1,5 +1,5 @@
 module "dev_infrastructure_sysdig" {
-  source = "github.com/ibm-garage-cloud/terraform-ibm-sysdig.git?ref=v1.1.0"
+  source = "github.com/ibm-garage-cloud/terraform-ibm-sysdig.git?ref=v2.0.0"
 
   resource_group_name      = module.dev_cluster.resource_group_name
   resource_location        = module.dev_cluster.region
@@ -8,6 +8,6 @@ module "dev_infrastructure_sysdig" {
   name_prefix              = var.name_prefix
   namespace                = module.dev_sre_namespace.name
   tags                     = [module.dev_cluster.tag]
-  exists                   = var.sysdig_exists == "true"
+  provision                = var.provision_sysdig == "true"
   name                     = var.sysdig_name
 }
