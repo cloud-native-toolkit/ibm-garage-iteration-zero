ee5a54f56d8145efe0539063746446dbe4a98657
Sean Sundberg <seansund@us.ibm.com>
2020-11-13 01:12:29 -0600

    Bumps ibm container-platform module to v1.18.0 and updates tile names (#222)
    * [create-pull-request] automated change
    
    * Bumps ibm container-platform module to v1.18.0 and updates tile names
    
    * Updates to the iZero modules
    
    - Updates the numbering of the tiles in the offering.json
    - Updates the default configuration values related to source control and the image registry in the tiles
    - Updates the default configuration in izero for the image registry
    - Updates the Artifactory module to 1.9.2 to pick up the cli version change
    
    * Removes image-tag from module
    
    * Adds entries for source_control module
    
    Co-authored-by: seansund <seansund@users.noreply.github.com>

--- terraform/stages/stage2-dashboard.tf
+++ terraform/stages/stage2-dashboard.tf
@@ -1,10 +1,9 @@
 module "dev_tools_dashboard" {
-  source = "github.com/ibm-garage-cloud/terraform-tools-dashboard.git?ref=v1.9.1"
+  source = "github.com/ibm-garage-cloud/terraform-tools-dashboard.git?ref=v1.10.0"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   cluster_type             = module.dev_cluster.type_code
   tls_secret_name          = module.dev_cluster.tls_secret_name
   releases_namespace       = module.dev_tools_namespace.name
-  image_tag                = "1.1.8"
 }
--- terraform/stages/stage2-image-registry.tf
+++ terraform/stages/stage2-image-registry.tf
@@ -1,18 +1,19 @@
 module "dev_tools_ibm_image_registry" {
-  source = "github.com/ibm-garage-cloud/terraform-ibm-image-registry.git?ref=v1.1.2"
+  source = "github.com/ibm-garage-cloud/terraform-ibm-image-registry.git?ref=v1.2.0"
 
   resource_group_name = module.dev_cluster.resource_group_name
   cluster_region      = module.dev_cluster.region
   cluster_type_code   = module.dev_cluster.type_code
   config_file_path    = module.dev_cluster.config_file_path
   cluster_namespace   = module.dev_tools_namespace.name
-  ibmcloud_api_key    = var.registry_password
+  ibmcloud_api_key    = var.ibmcloud_api_key
+  registry_password   = var.registry_password
   registry_namespace  = var.registry_namespace
   apply               = var.registry_type == "icr"
 }
 
 module "dev_tools_ocp_image_registry" {
-  source = "github.com/ibm-garage-cloud/terraform-ocp-image-registry.git?ref=v1.1.3"
+  source = "github.com/ibm-garage-cloud/terraform-ocp-image-registry.git?ref=v1.2.0"
 
   cluster_type_code   = module.dev_cluster.type_code
   config_file_path    = module.dev_cluster.config_file_path
