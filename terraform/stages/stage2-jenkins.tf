994c3a7435300c8519390c8472df8067492a3876
seansund <seansund@users.noreply.github.com>
2020-12-08 20:47:20 +0000

    [create-pull-request] automated change

--- terraform/stages/stage2-jenkins.tf
+++ terraform/stages/stage2-jenkins.tf
@@ -1,5 +1,5 @@
 module "dev_tools_jenkins" {
-  source = "github.com/ibm-garage-cloud/terraform-tools-jenkins.git?ref=v1.4.2"
+  source = "github.com/ibm-garage-cloud/terraform-tools-jenkins.git?ref=v1.4.3"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
