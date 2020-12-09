b85d00e80f624a0f1ee02f1b1f03efa1fefd0689
Sean Sundberg <seansund@us.ibm.com>
2020-12-09 09:16:44 -0600

    Adds new dispatch workflow to create a PR for an individual module

--- .github/scripts/update-modules.sh
+++ .github/scripts/update-modules.sh
@@ -7,9 +7,16 @@ if [[ -n "$GITHUB_USERNAME" ]] && [[ -n "$GITHUB_TOKEN" ]]; then
   GITHUB_AUTH="-u ${GITHUB_USERNAME}:${GITHUB_TOKEN}"
 fi
 
+CHANGED_REPO="$1"
+
+if [[ -n "${CHANGED_REPO}" ]]; then
+  echo "** Updating modules based on filter: ${CHANGED_REPO}"
+else
+  echo "** Updating all modules"
+fi
+
 set -e
 
-echo "Reading stages from dir: ${REPO_DIR}/terraform"
 ls "${REPO_DIR}/terraform" | while read -r dir; do
   if [[ ! -d "${REPO_DIR}/terraform/${dir}" ]] || [[ ! "${dir}" =~ stages* ]]; then
     continue
@@ -17,15 +24,14 @@ ls "${REPO_DIR}/terraform" | while read -r dir; do
 
   STAGES_DIR="${REPO_DIR}/terraform/${dir}"
 
-  echo " - Reading terraform stages: ${STAGES_DIR}"
+  echo "Reading terraform stages: ${STAGES_DIR}"
   ls "${STAGES_DIR}" | while read -r file; do
     stageFile="${STAGES_DIR}/${file}"
 
     if [[ ! -f "${stageFile}" ]] || [[ ! "${stageFile}" =~ .*tf$ ]]; then
-      echo "Skipping non stage file: ${stageFile}"
+      continue
     fi
 
-    echo "Updating stage: $stageFile"
     SOURCES=$(grep -E 'source *=' "${stageFile}" | sed -E 's/.*source *= *"(.*)"/\1/g')
 
     echo "${SOURCES}" | while read -r source; do
@@ -34,6 +40,13 @@ ls "${REPO_DIR}/terraform" | while read -r dir; do
       fi
 
       git_slug=$(echo "$source" | sed -E 's~github.com/(.*)\?.*~\1~g' | sed "s/.git//g")
+
+      if [[ -z "${CHANGED_REPO}" ]] || [[ "${git_slug}" == "${CHANGED_REPO}" ]]; then
+        echo " - Updating stage: $stageFile"
+        echo " - Checking for latest version of ${git_slug}"
+      else
+        continue
+      fi
 
       git_release_url="https://api.github.com/repos/${git_slug}/releases/latest"
 
