7d77ee966981323c0a9ffc0a54e0ef3b2cf8599e
seansund <seansund@users.noreply.github.com>
2021-01-29 04:41:36 +0000

    Updates ibm-garage-cloud/terraform-ibm-container-platform module to v1.20.1

--- terraform/stages/stage1-cluster.tf
+++ terraform/stages/stage1-cluster.tf
@@ -1,5 +1,5 @@
 module "dev_cluster" {
-  source = "github.com/ibm-garage-cloud/terraform-ibm-container-platform.git?ref=v1.20.0"
+  source = "github.com/ibm-garage-cloud/terraform-ibm-container-platform.git?ref=v1.20.1"
 
   resource_group_name     = var.resource_group_name
   cluster_name            = var.cluster_name
