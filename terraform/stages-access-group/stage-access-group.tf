d8301f4f60df8cdd186dd1f3c27ef4876d0001d0
Sean Sundberg <seansund@us.ibm.com>
2021-02-19 00:10:54 -0600

    Updates variables for access group tile

--- terraform/stages-access-group/stage-access-group.tf
+++ terraform/stages-access-group/stage-access-group.tf
@@ -2,5 +2,5 @@ module "access_group" {
   source = "github.com/ibm-garage-cloud/terraform-ibm-access-group.git?ref=v1.2.1"
 
   resourceGroupNames   = split(",", var.resource_group_names)
-  createResourceGroups = var.create_resource_groups
+  createResourceGroups = var.create_resource_groups == "true"
 }
