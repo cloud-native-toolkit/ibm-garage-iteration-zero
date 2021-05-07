237474a83ac0a19465f5f032e6893c7c7534366b
cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
2021-05-07 02:23:03 +0000

    Updates cloud-native-toolkit/terraform-tools-dashboard module to v1.10.12
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/stages/stage2-dashboard.tf
+++ terraform/stages/stage2-dashboard.tf
@@ -1,5 +1,5 @@
 module "dev_tools_dashboard" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-dashboard.git?ref=v1.10.8"
+  source = "github.com/cloud-native-toolkit/terraform-tools-dashboard.git?ref=v1.10.12"
 
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
