09564c3c74b9f408d31d32a3fc60654f01e9f6d2
Sean Sundberg <seansund@us.ibm.com>
2021-11-03 18:19:27 -0500

    Updates version of access_group module
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- terraform/stages/stage1-access_group.tf
+++ terraform/stages/stage1-access_group.tf
@@ -1,5 +1,5 @@
 module "access_groups" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-access-group.git?ref=v1.0.0"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-access-group.git?ref=v3.0.0"
 
   resource_group_name  = module.resource_group.name
 }
