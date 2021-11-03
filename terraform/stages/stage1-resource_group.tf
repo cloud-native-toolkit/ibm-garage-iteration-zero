7c083e5d97046ec2ddb3785fbdca357f02ed2c45
Sean Sundberg <seansund@us.ibm.com>
2021-11-03 18:28:16 -0500

    Updates resource group version
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- terraform/stages/stage1-resource_group.tf
+++ terraform/stages/stage1-resource_group.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git?ref=v2.4.6"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git?ref=v3.0.0"
 
   provision           = var.resource_group_provision
   resource_group_name = var.resource_group_name
