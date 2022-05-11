0955c1adaa05ce3f36c63d33465b331de1364338
cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
2022-05-11 19:54:28 +0000

    Updates cloud-native-toolkit/terraform-k8s-namespace module to v3.2.3
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/stages-ocp4/stage1-namespaces.tf
+++ terraform/stages-ocp4/stage1-namespaces.tf
@@ -1,19 +1,19 @@
 module "dev_tools_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git?ref=v3.2.2"
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git?ref=v3.2.3"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
   name                     = var.tools_namespace
 }
 
 module "dev_sre_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git?ref=v3.2.2"
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git?ref=v3.2.3"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
   name                     = var.sre_namespace
 }
 
 module "toolkit_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git?ref=v3.2.2"
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git?ref=v3.2.3"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
   name                     = var.toolkit_namespace
