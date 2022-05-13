bf2217f1834b8424622cadbf85a29f533e27d364
Cloud-Native Toolkit <79655631+cloudnativetoolkit@users.noreply.github.com>
2022-05-13 08:32:01 -0500

    Updates cloud-native-toolkit/terraform-tools-tekton-resources module to v2.3.2 (#408)

--- terraform/stages-ocp4/stage2-pipelines.tf
+++ terraform/stages-ocp4/stage2-pipelines.tf
@@ -1,5 +1,5 @@
 module "dev_tools_tekton_resources" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-tekton-resources.git?ref=v2.3.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-tekton-resources.git?ref=v2.3.2"
 
   cluster_type             = module.dev_cluster.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
