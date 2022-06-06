3f99691ba33522bc1fbfbd3aa555af35aef7d4e0
cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
2022-06-06 16:17:00 +0000

    Updates cloud-native-toolkit/terraform-ibm-account-users module to v1.1.1
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/stages-invite-users/stage-access-group.tf
+++ terraform/stages-invite-users/stage-access-group.tf
@@ -1,5 +1,5 @@
 module "invite_users" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-account-users.git?ref=v1.1.0"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-account-users.git?ref=v1.1.1"
 
   users = split(",", var.users)
 }
