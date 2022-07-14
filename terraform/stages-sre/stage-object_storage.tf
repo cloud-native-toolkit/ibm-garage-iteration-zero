5d9ae358bad4f6a2b5838afd1321070075cede5d
cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
2022-07-14 18:28:38 +0000

    Updates cloud-native-toolkit/terraform-ibm-object-storage module to v4.0.6
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/stages-sre/stage-object_storage.tf
+++ terraform/stages-sre/stage-object_storage.tf
@@ -1,5 +1,5 @@
 module "dev_infrastructure_cos" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-object-storage?ref=v4.0.0"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-object-storage?ref=v4.0.6"
 
   resource_group_name = var.resource_group_name
   resource_location   = "global"
