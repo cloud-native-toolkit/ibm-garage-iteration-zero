a0dd912869846bdce44ea6013ccaa2c87d184622
cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
2023-07-08 04:34:33 +0000

    Updates cloud-native-toolkit/terraform-tools-openshift-cicd module to v2.0.1
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/stages-ocp4/stage2-cicd.tf
+++ terraform/stages-ocp4/stage2-cicd.tf
@@ -1,5 +1,5 @@
 module "cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.11.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v2.0.1"
 
   cluster_config_file = module.dev_cluster.config_file_path
   cluster_type        = module.dev_cluster.type_code
